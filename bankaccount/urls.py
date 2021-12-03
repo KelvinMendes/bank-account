afaa4c6ff2f7ff84b3fbdf8319fec5a3b5cad79f
Kelvin Mendes <k.mendes95@outlook.com>
2021-12-03 14:14:00 -0300

    bankaccountupdated

--- bankaccount/urls.py
+++ bankaccount/urls.py
@@ -1,17 +1,18 @@
 from django.contrib import admin
 from django.db import router
 from django.urls import path, include
-from bankaccountapp.views import ClientsViewSet, AccountViewSet
+from bankaccountapp.views import ClientsViewSet, TransactionViewSet
 from rest_framework import routers
 
 
 router = routers.DefaultRouter()
 router.register('clients', ClientsViewSet, basename='Clients')
-router.register('accounts', AccountViewSet, basename='Accounts')
+#router.register('transactions', TransactionViewSet, basename='Transactions')
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
+    path('transactions', TransactionViewSet.as_view()),
 ]
